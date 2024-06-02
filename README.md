c8a13142f66ec2b29e11c53158f603f9d6fc6f9a
탁민주 <takminjoo@tagminjuui-MacBookAir.local>
2024-06-02 23:01:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# min_99 React 보일러플레이트 프로젝트 입니다.
+
+- yarn 4.1.1
+- typescript
+- React : 18.2.0
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>min_99의 boilerplate 프로젝트 입니다.</div>;
+}
+
+export default App;
